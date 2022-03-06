dbd558aae91c8de3b50c1bc2e511ff70056bc5de
unknown <adrianmolina39@gmail.com>
2022-03-06 16:46:11 -0400

    Fixing bugs

--- src/components/Main/Welcome.js
+++ src/components/Main/Welcome.js
@@ -38,11 +38,11 @@ const Welcome = (number) => {
                 axios.get(`/bill/paid`)
                     .then((resp) => {
 
-
+                        
                         axios.get(`/bill/notPayed`)
                             .then((respuesta) => {
-                                setState({ ...state, porPagar: res.data, pagadas: resp.data, vencidas: respuesta.data })
-
+                                setState({ ...state, porPagar: res.data.length, pagadas: resp.data.data.length, vencidas: respuesta.data.length })
+                           
                             })
                             .catch((error) => console.log(error))
 
@@ -54,7 +54,6 @@ const Welcome = (number) => {
 
 
 
-        //eslint-disable-next-line
     }, [])
 
 
@@ -65,9 +64,9 @@ const Welcome = (number) => {
             <div>
                 <p> <span className='diadehoy'>  El dia de hoy <b> {hoy}</b> <br />  </span>
                     Tenemos un total de:
-                    <span className='text-success' > <br />-{state.pagadas.length} facturas pagadas
-                    </span>  <br />  <span className='text-primary'>-{state.porPagar.length} facturas por pagar</span>
-                    <br /><span className='text-danger'>-{state.vencidas.length} facturas vencidas  </span>  </p>
+                    <span className='text-success' > <br />-{state.pagadas} facturas pagadas
+                    </span>  <br />  <span className='text-primary'>-{state.porPagar} facturas por pagar</span>
+                    <br /><span className='text-danger'>-{state.vencidas} facturas vencidas  </span>  </p>
 
             </div>
 
